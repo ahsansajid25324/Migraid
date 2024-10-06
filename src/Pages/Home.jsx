46266ba09c4dfd5c682b3327b51786b71cd3b4cd
Ahsan Sajid <sajidahsan67@gmail.com>
2024-10-06 17:10:34 +0500

    2nd commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Heading, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <Heading>FdfdsfdsDS</Heading>
+        <Text>fsdf</Text>
+      
+    </div>
+  )
+}
+
+export default Home
