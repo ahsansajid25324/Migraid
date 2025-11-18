125e9efa55825f6cefd6891f97054156de86f5e0
Ahsa Sajid <sajidahsan67@gmail.com>
2025-11-18 20:06:13 +0500

    update team

--- src/Components/Pages/TeamSection.jsx
+++ src/Components/Pages/TeamSection.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { Box, Flex, Grid, Image, Text } from "@chakra-ui/react";
 import SectionHeading from "./SectionHeading";
-import { teamData } from "../../data/TeamData";
 
 import Line from "../../assets/images/Line.png";
+
+import Hurr from "../../assets/images/hurr.JPG";
+import Joseph from "../../assets/images/joseph.jpeg";
+export const teamData = [
+  {
+    name: "Hurr Murtaza",
+    role: "Immigration specialist",
+    image: Hurr,
+  },
+  {
+    name: "Joseph Soliz",
+    role: "Immigration specialist",
+    image: Joseph,
+  },
+];
 
 const TeamSection = () => {
   return (
@@ -23,42 +37,42 @@ const TeamSection = () => {
         />
       </Box>
 
-      <Grid
-            px={{ base: 4, lg: 12 }}
-
-        templateColumns={{
-          base: "repeat(1, 1fr)",
-          md: "repeat(2, 1fr)",
-          lg: "repeat(4, 1fr)",
-        }}
-        py={{base:0,lg:6}}
-      >
-        {teamData.map((member, index) => (
-          <Box   key={index} textAlign="center" py={{ base: 4, lg: 0 }} >
-            <Image
-              src={member.image}
-              alt={member.name}
-              borderRadius="full"
-              border="2px solid rgba(34, 185, 116, 1)"
-              boxSize="160px"
-              mx="auto"
-              p={2}
-              mb={4}
-              objectFit="cover"
-            />
-            {/* Member Details */}
-            <Flex flexDirection={"column"} justifyItems={"center"}>
-              <Text fontWeight="bold" fontSize="lg" mb={3}>
-                {member.name}
-              </Text>
-              <Image m="auto" w="15%" h="2px" src={Line}></Image>
-              <Text fontSize="sm" mt={3} color="gray.500">
-                {member.role}
-              </Text>
-            </Flex>
-          </Box>
-        ))}
-      </Grid>
+      <Box maxW="1200px" mx="auto">
+        <Grid
+          px={{ base: 4, lg: 8 }}
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            md: "repeat(2, 1fr)",
+          }}
+          gap={12}
+          py={{ base: 0, lg: 6 }}
+        >
+          {teamData.map((member, index) => (
+            <Box key={index} textAlign="center">
+              <Image
+                src={member.image}
+                alt={member.name}
+                borderRadius="xl"
+                border="4px solid rgba(34, 185, 116, 1)"
+                boxSize="300px"
+                mx="auto"
+                mb={4}
+                objectFit="cover"
+              />
+              {/* Member Details */}
+              <Flex flexDirection={"column"} justifyItems={"center"}>
+                <Text fontWeight="bold" fontSize="lg" mb={3}>
+                  {member.name}
+                </Text>
+                <Image m="auto" w="15%" h="2px" src={Line}></Image>
+                <Text fontSize="sm" mt={3} color="gray.500">
+                  {member.role}
+                </Text>
+              </Flex>
+            </Box>
+          ))}
+        </Grid>
+      </Box>
     </Flex>
   );
 };
